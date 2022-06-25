bb07572fd2f61835716fdcf0d4e34f52f208b057
wafflejuice <wafflejuice96@gmail.com>
2022-06-25 22:29:01 +0900

    PS

---
+++ src/_2798_블랙잭.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        int closest_sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum <= m) {
+                        if (closest_sum < sum) {
+                            closest_sum = sum;
+                        }
+                    }
+                }
+            }
+        }
+
+        bw.write(Integer.toString(closest_sum));
+        bw.flush();
+        bw.close();
+    }
+}
