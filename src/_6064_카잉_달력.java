07363d3265c8b8db3b185a98239b88971742824b
wafflejuice <wafflejuice96@gmail.com>
2022-06-30 07:07:03 +0900

    PS

---
+++ src/_6064_카잉_달력.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _6064_카잉_달력 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int t = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int answer = -1;
+            int year = x;
+            int[] cain = new int[2];
+
+            while (year <= m * n) {
+                toCain(year, m, n, cain);
+
+                if (cain[0] == x && cain[1] == y) {
+                    answer = year;
+                    break;
+                }
+
+                if (cain[0] < x) {
+                    year += x - cain[0];
+                } else if (cain[1] < y) {
+                    year += y - cain[1];
+                } else if (x < cain[0]) {
+                    year += m - cain[0] + 1;
+                } else {
+                    year += n - cain[1] + 1;
+                }
+            }
+
+            System.out.println(answer);
+        }
+    }
+
+    static boolean toCain(int year, int m, int n, int[] cain) {
+        int x = year % m;
+        if (x == 0) {
+            x = m;
+        }
+
+        int y = year % n;
+        if (y == 0) {
+            y = n;
+        }
+
+        cain[0] = x;
+        cain[1] = y;
+
+        return true;
+    }
+}
