8878b4326d33f2da782a81ca6bc17a27cd5184b8
wafflejuice <wafflejuice96@gmail.com>
2022-10-29 22:54:58 +0900

    724. Find Pivot Index.kt

--- src/leetcode/1480. Running Sum of 1d Array.kt
+++ src/leetcode/1480. Running Sum of 1d Array.kt
@@ -2,18 +2,16 @@ package leetcode
 
 import kotlin.test.assertContentEquals
 
-class Solution {
-    fun runningSum(nums: IntArray): IntArray {
-        var acc = 0
-        return nums.map {
-            acc += it
-            acc
-        }.toIntArray()
-    }
+fun runningSum(nums: IntArray): IntArray {
+    var acc = 0
+    return nums.map {
+        acc += it
+        acc
+    }.toIntArray()
 }
 
 fun main() {
-    assertContentEquals(Solution().runningSum(listOf(1,2,3,4).toIntArray()), listOf(1,3,6,10).toIntArray())
-    assertContentEquals(Solution().runningSum(listOf(1,1,1,1,1).toIntArray()), listOf(1,2,3,4,5).toIntArray())
-    assertContentEquals(Solution().runningSum(listOf(3,1,2,10,1).toIntArray()), listOf(3,4,6,16,17).toIntArray())
+    assertContentEquals(runningSum(listOf(1,2,3,4).toIntArray()), listOf(1,3,6,10).toIntArray())
+    assertContentEquals(runningSum(listOf(1,1,1,1,1).toIntArray()), listOf(1,2,3,4,5).toIntArray())
+    assertContentEquals(runningSum(listOf(3,1,2,10,1).toIntArray()), listOf(3,4,6,16,17).toIntArray())
 }
---
+++ src/leetcode/724. Find Pivot Index.kt
@@ -0,0 +1,35 @@
+package leetcode
+
+import kotlin.test.assertEquals
+
+fun pivotIndex(nums: IntArray): Int {
+    val size = nums.size
+
+    val ls = Array(size) {0}
+    var leftAcc = 0
+    for (i in 0 until size) {
+        ls[i] = leftAcc
+        leftAcc += nums[i]
+    }
+
+    val rs = Array(size) {0}
+    var rightAcc = 0
+    for (i in (size-1) downTo 0) {
+        rs[i] = rightAcc
+        rightAcc += nums[i]
+    }
+
+    for (i in 0 until size) {
+        if (ls[i] == rs[i]) {
+            return i
+        }
+    }
+
+    return -1
+}
+
+fun main() {
+    assertEquals(3, pivotIndex(intArrayOf(1,7,3,6,5,6)))
+    assertEquals(-1, pivotIndex(intArrayOf(1,2,3)))
+    assertEquals(0, pivotIndex(intArrayOf(2,1,-1)))
+}
